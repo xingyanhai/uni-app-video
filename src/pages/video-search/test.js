c130e788b17097b4ec57dcf4e9e7407c7dc1ece4
1101099728@qq.com <142xyh753869>
2020-08-24 17:06:09 +0800

    test

--- src/pages/video-search/test.js
+++ src/pages/video-search/test.js
@@ -1,5 +1,16 @@
+// 天天在线
 // https://wq.432115.com/app/index.php?i=12&t=0&v=2.0.8&from=wxapp&c=entry&a=wxapp&do=Search&state=we7sid-077b2e6899b9f30bec5fdcb0777953a7&m=sg_movie&sign=301dcf4766d83a0f2f651ba2d7111b71&key=%E5%93%88
 // https://wq.432115.com/app/index.php?i=12&t=0&v=2.0.8&from=wxapp&c=entry&a=wxapp&do=Search&state=we7sid-077b2e6899b9f30bec5fdcb0777953a7&m=sg_movie&sign=62c2bfa7cd7f5fc3df5c4fab271d7cd3&key=%E8%82%96%E7%94%B3%E5%85%8B
 // https://wq.432115.com/app/index.php?i=12&t=0&v=2.0.8&from=wxapp&c=entry&a=wxapp&do=Search&state=we7sid-077b2e6899b9f30bec5fdcb0777953a7&m=sg_movie&sign=0f02ee28eddb671bac73955e99ea2114&key=%E5%8D%81%E4%BA%8C
 
 // https://wq.432115.com/app/index.php?i=12&t=0&v=2.0.8&from=wxapp&c=entry&a=wxapp&do=GetVideo&state=we7sid-077b2e6899b9f30bec5fdcb0777953a7&m=sg_movie&sign=8e86f2a2338388ad0184bc3d16ef85ff&id=8256
+
+
+
+
+// 知也电影
+// 1 https://www.zydy365.com/mini/cms/public/index.php?service=App.Mini.searchVod&key=%E5%85%AB%E4%BD%B0
+// 2 https://www.zydy365.com/mini/cms/public/index.php?service=App.Mini.getOnlineMvByIdZHIYE2&vodid=93188
+
+// 在线会员热门电影
+// 1.https://www.iyx3.com/app/index.php?i=2&t=0&v=1.1&from=wxapp&c=entry&a=wxapp&do=Search&m=sg_movie&key=%E5%85%AB%E4%BD%B0
